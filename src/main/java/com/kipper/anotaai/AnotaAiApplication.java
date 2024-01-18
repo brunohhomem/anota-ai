64fad4e95e16a4000de66d08f0e02c0ab4c09396
Bruno H. Homem <brunohhomem@gmail.com>
2024-01-18 16:18:48 -0300

    feat: Creation entity and repository for category and product

--- src/main/java/com/kipper/anotaai/AnotaAiApplication.java
+++ src/main/java/com/kipper/anotaai/AnotaAiApplication.java
@@ -2,8 +2,10 @@ package com.kipper.anotaai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class AnotaAiApplication {
 
 	public static void main(String[] args) {
